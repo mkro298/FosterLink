665f5a8bc447e5ff0f39821b567f5c847f6443cb
mrinalini-ganesh <117322533+mrinalini-ganesh@users.noreply.github.com>
2024-07-23 16:23:13 -0700

    Update table.sql

--- database/table.sql
+++ database/table.sql
@@ -24,26 +24,6 @@ CREATE TABLE Tutors (
     CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE Scholarships (
-    ScholarshipID INT AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(100),
-    EligibilityCriteria TEXT,
-    ApplicationDeadline DATE,
-    Amount DECIMAL(10, 2),
-    ContactInfo VARCHAR(100),
-    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-
-CREATE TABLE Resources (
-    ResourceID INT AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(100),
-    URL VARCHAR(255),
-    Category VARCHAR(50),  -- e.g., "Educational", "Mental Health"
-    Description TEXT,
-    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
 
 CREATE TABLE ChatMessages (
     MessageID INT AUTO_INCREMENT PRIMARY KEY,
